748492b109584419955ed1bcee6409e17112467b
Nishit <138427125+Nishit3116@users.noreply.github.com>
2023-07-03 21:19:27 +0530

    Add files via upload

---
+++ swapv3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+// For swapinf3 var
+
+
+{
+	
+	
+	int a,b;
+	
+	printf("enter the value of a = ");
+	scanf("%d",&a);
+	printf("enter the value of b = ");
+	scanf("%d",&b);
+	
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	
+	printf("value of a = %d\n",a);
+	printf("value of b = %d",b);
+	
+	
+	
+}
