1841aca91f08d7f23b4ce03ce89373ab6bc7c389
Nishit <138427125+Nishit3116@users.noreply.github.com>
2023-07-03 21:18:02 +0530

    Add files via upload

---
+++ scanfabc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+//For scanf abc
+	
+	int a,b,c;
+	
+	printf("enter the value of a = ");
+	scanf("%d",&a);
+	
+	printf("enter the value of b = ");
+	scanf("%d",&b);
+	
+	c = a;
+	a = b;
+	b = c;
+	
+	printf("value of a = %d\n",a);
+	printf("value of b = %d",b);
+	
+	
+	
+}
